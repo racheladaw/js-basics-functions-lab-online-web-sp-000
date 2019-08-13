5f95052cf2440e3ac861c704a02686efef5c9672
Rachel <williamsrachelada@gmail.com>
2019-08-12 22:47:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  if (streetNumber > 42) {
+    return streetNumber - 42;
+  } else {
+    return 42 - streetNumber;
+  }
+}
+
+function distanceFromHqInFeet(streetNumber) {
+  return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function  distanceTravelledInFeet(startingBlock, endingBlock) {
+  if (startingBlock > endingBlock) {
+    return (startingBlock - endingBlock) * 264;
+  } else {
+    return (endingBlock - startingBlock) *264;
+  }
+}
+
+function  calculatesFarePrice(start, destination) {
+  let distanceTravelled = distanceTravelledInFeet(start, destination);
+  if (distanceTravelled > 2500) {
+    return "cannot travel that far";
+  } else if (distanceTravelled > 2000) {
+    return 25;
+  } else if (distanceTravelled > 400) {
+    return (distanceTravelled  - 400) * 0.02;
+  } else {
+    return 0;
+  }
+}
